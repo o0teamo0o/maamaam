f5d5c24a7f48d5db476a97b77691562107884c79
o0teamo0o <121828403@qq.com>
2019-07-30 17:28:41 +0800

    修整接口

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -253,8 +253,11 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
-
+  onShow: function () {
+    var that = this;
+    that.setData({
+      userInfo: app.globalData.userInfo,
+    })
   },
 
   /**
